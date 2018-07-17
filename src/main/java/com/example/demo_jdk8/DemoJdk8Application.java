7856f0c4d9e35793d7da91324859ef0d4cb7fe00
hekai <hekai@jshuii.com>
2018-07-17 11:32:34 +0800

    create project

---
+++ src/main/java/com/example/demo_jdk8/DemoJdk8Application.java
@@ -0,0 +1,12 @@
+package com.example.demo_jdk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJdk8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJdk8Application.class, args);
+	}
+}
